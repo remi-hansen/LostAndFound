7097f52a809c810b049a6fc93abaaee6dde024c0
Remi Hansen <hansenrd@bc.edu>
2022-12-06 20:00:44 -0500

    Initial Commit

---
+++ LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Remi Pacifico Hansen on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
